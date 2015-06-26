c78c32b7c3d98a80ae15845a610dd5ff49d36581
Christian Ranz <cr@cr.fritz.box>
2015-06-26 14:31:25 +0200

    Include dependency sinon-as-promised

--- core/index.js
+++ core/index.js
@@ -1,4 +1,5 @@
 var referee = require('referee'), sinon = require('sinon');
+require('sinon-as-promised');
 referee.format = require('formatio').configure({quoteStrings: false}).ascii;
 require('referee-sinon')(referee, sinon);
 
